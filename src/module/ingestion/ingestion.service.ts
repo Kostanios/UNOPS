71b9fc3a76092ce65c0e14476a48252c7364d8b8
KonstantinKotau <kotov.const@gmail.com>
2025-05-12 22:06:55 +0200

    stream fix

--- src/module/ingestion/ingestion.service.ts
+++ src/module/ingestion/ingestion.service.ts
@@ -1,28 +1,34 @@
 import { Injectable } from '@nestjs/common';
-import * as JSONStream from 'stream-json';
-import * as StreamArray from 'stream-json/streamers/StreamArray';
 import axios from 'axios';
-import { ListingService } from '../data/listing.service';
-import { AccommodationService } from '../data/accommodation.service';
+import * as StreamArray from 'stream-json/streamers/StreamArray';
+import { pipeline } from 'stream/promises';
+import { Readable } from 'stream';
 import { DataType } from '../../enum/data.enum';
+import { AccommodationService } from '../data/accommodation.service';
+import { ListingService } from '../data/listing.service';
 
 @Injectable()
 export class IngestionService {
   constructor(
     private readonly accommodationService: AccommodationService,
-    private readonly listingService: ListingService,
-  ) {}
+    private readonly listingService: ListingService
+) {}
 
   async ingestFromUrl(url: string, sourceType: DataType) {
     const response = await axios.get(url, { responseType: 'stream' });
-    const pipeline = response.data.pipe(JSONStream.parser()).pipe(StreamArray.withParser());
 
-    for await (const { value } of pipeline) {
-      if (sourceType === 'accommodation') {
-        await this.accommodationService.save(value);
-      } else {
-        await this.listingService.save(value);
+    const jsonStream = StreamArray.withParser();
+
+    const processing = async () => {
+      for await (const { value } of jsonStream) {
+        if (sourceType === DataType.Accommodation) await this.accommodationService.save(value);
+        else if (sourceType === DataType.Listing) await this.listingService.save(value);
       }
-    }
+    };
+
+    await Promise.all([
+      pipeline(response.data as Readable, jsonStream),
+      processing(),
+    ]);
   }
 }
